9abed5da50a0335b945c0b9aa2cbdddf81ff9d12
Brendan <52293987+Digital-Brendan@users.noreply.github.com>
2026-03-09 21:28:25 +0000

    Complete ex 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  return Number(((farenheit - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return Number(((celcius * 1.8) + 32).toFixed(1));
 };
 
 // Do not edit below this line
