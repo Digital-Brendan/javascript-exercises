336312dcc93682b99a2b50cb2c0029322e96deed
Brendan <52293987+Digital-Brendan@users.noreply.github.com>
2026-03-09 21:19:37 +0000

    Complete ex 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    if (!Number.isInteger(from) || !Number.isInteger(to)) return "ERROR";
+    if (from <= 0 || to <= 0) return "ERROR";
 
+    let sum = 0;
+    const minimum = Math.min(from, to);
+    const maximum = Math.max(from, to);
+
+    for (let i = minimum; i <= maximum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
