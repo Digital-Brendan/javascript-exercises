96a6c430e9c1952740cf5d68705b2eb87973f370
Brendan <52293987+Digital-Brendan@users.noreply.github.com>
2026-03-24 16:20:09 +0000

    Finish 12 and 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((char) => alphanum.includes(char))
+    .join('')
+
+    const reversedStr = cleanedStr.split('').reverse().join('');
+
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
