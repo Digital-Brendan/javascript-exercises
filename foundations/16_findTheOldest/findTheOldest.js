b4e0dfcea03e132fed334b8d9e28f629a8cc976e
Brendan <52293987+Digital-Brendan@users.noreply.github.com>
2026-03-24 16:41:49 +0000

    Finish foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+    person.yearOfDeath ??= new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    const sortedPeople = people.toSorted(
+        (currentPerson, nextPerson) => getAge(nextPerson) - getAge(currentPerson)
+    );
+
+    const oldestPerson = sortedPeople[0];
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
