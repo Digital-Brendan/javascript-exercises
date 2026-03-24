1ae4206b372a1b5efdcbe5968f527f4a1053648a
Brendan <52293987+Digital-Brendan@users.noreply.github.com>
2026-03-24 16:42:17 +0000

    Finish exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,24 +1,24 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
 };
 
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const numericNum = parseInt(num);
 
+    if (numericNum < 0) return "OOPS";
+    if (numericNum == 0) return 0;
+
+    let firstPrevious = 1;
+    let secondPrevious = 0;
+
+    for (let i = 2; i <= num; i++) {
+        let current = firstPrevious + secondPrevious;
+        secondPrevious = firstPrevious;
+        firstPrevious = current;
+    }
+
+    return firstPrevious;
 };
 
 // Do not edit below this line
